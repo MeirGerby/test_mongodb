34577b314211e07fcb48598310d505de1def2e52
Meir-Gerby <meirg2001@gmail.com>
2026-02-05 12:50:50 +0200

    test

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI 
-from routes import router 
+import routes 
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(routes.router)
 
 
 if __name__ == "__main__":
